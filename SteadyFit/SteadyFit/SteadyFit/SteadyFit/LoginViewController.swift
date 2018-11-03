dfac281b5d812ca8a6d571064a0d0634a120a61f
alexachen <alexac@sfu.ca>
2018-11-03 02:15:41 -0700

    login init

---
+++ SteadyFit/SteadyFit/SteadyFit/SteadyFit/LoginViewController.swift
@@ -0,0 +1,32 @@
+//
+//  LoginViewController.swift
+//  SteadyFit
+//
+//  Created by Alexa Chen on 2018-11-03.
+//  Copyright © 2018 Raheem Mian. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBAction func SignInRegisterSelector(_ sender: Any) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
