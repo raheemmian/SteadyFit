d805e5898d35b33f50730c53c193e7f1195bcee2
alexachen <alexac@sfu.ca>
2018-11-02 19:47:51 -0700

    loaded groups from firebase for user1

---
+++ SteadyFit/SteadyFit/SteadyFit/Model/userGroup.swift
@@ -0,0 +1,15 @@
+//
+//  Group.swift
+//  SteadyFit
+//
+//  Created by Alexa Chen on 2018-11-02.
+//  Copyright © 2018 Raheem Mian. All rights reserved.
+//
+
+import UIKit
+
+class userGroup: NSObject {
+    var name: String?
+    var chatid: String?
+    var GroupType: String?
+}
