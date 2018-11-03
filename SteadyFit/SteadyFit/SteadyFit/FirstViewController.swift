4c1bf28f4d17d333994bad1ef54f558ef0cdd1e2
raheemmian <rmian@sfu.ca>
2018-11-02 20:39:26 -0700

    changed first storyboard

--- SteadyFit/SteadyFit/SteadyFit/FirstViewController.swift
+++ SteadyFit/SteadyFit/SteadyFit/FirstViewController.swift
@@ -8,43 +8,76 @@
 
 //Home
 import UIKit
+import MessageUI
+import MapKit
+import CoreLocation
+class FirstViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, MFMessageComposeViewControllerDelegate, CLLocationManagerDelegate{
+    var myindex = 0;
+    var locationManager = CLLocationManager()
+    
+    var groups = [[String]]()
+    let items = [ ["Histogram"] , ["dog", "cat", "cc"]]
+    let sections = ["Activity Tracker", "Events"]
+    
 
-class FirstViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
-    var events = ["dog", "cat"]
-    var tableFooterView: UIView?
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return events.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let tableCell = UITableViewCell(style: UITableViewCell.CellStyle.default
-            , reuseIdentifier: "cell")
-        tableCell.textLabel?.text = events[indexPath.row]
-        return tableCell
-    }
-    
-    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return "Events"
-    }
-    
-    @IBOutlet weak var table: UITableView!
-    
-    @IBOutlet weak var ActivityTracker: UIButton!
+    @IBAction func EmergencyButton(_ sender: Any) {sendText()}
+    @IBOutlet weak var myTableView: UITableView!
     @IBOutlet weak var city: UILabel!
     @IBOutlet weak var name: UILabel!
-    @IBOutlet var profilePicture: UIImageView!
+    @IBOutlet weak var profilePicture: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        myTableView.delegate = self
+        myTableView.dataSource = self
         profilePicture.layer.cornerRadius = profilePicture.frame.size.width / 2
         profilePicture.clipsToBounds = true
-        ActivityTracker.layer.borderWidth = 1.0;
-        //name.layer.borderWidth = 1.0
-      
-        
-
+        self.locationManager.requestWhenInUseAuthorization()
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+        }
     }
-
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let locValue:CLLocationCoordinate2D = manager.location!.coordinate
+       // print("location = \(locValue.)
+    }
+    func messageComposeViewController(_ controller: MFMessageComposeViewController, didFinishWith result: MessageComposeResult) {
+        controller.dismiss(animated: true, completion: nil)
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return sections.count
+    }
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sections[section]
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items[section].count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell  = tableView.dequeueReusableCell(withIdentifier: "cell", for:  indexPath)
+        cell.textLabel?.text = items[indexPath.section][indexPath.row]
+        return cell
+    }
+   func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: sections[indexPath.section], sender: self)
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    func sendText() {
+        print("hi")
+        let composeVC = MFMessageComposeViewController()
+        composeVC.messageComposeDelegate = self
+        composeVC.recipients = ["7788823644"]
+        composeVC.body = "test!"
+        if MFMessageComposeViewController.canSendText() {
+            self.present(composeVC, animated: true, completion: nil)
+        } else {
+            print("Can't send messages.")
+        }
+    }
 
 }
 
