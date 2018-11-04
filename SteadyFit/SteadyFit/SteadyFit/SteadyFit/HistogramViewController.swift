da387478e09d2a3ce29b851d832dcf7083303711
raheemmian <rmian@sfu.ca>
2018-11-03 17:05:53 -0700

    finished profile and friends
    added events headers
    gps location
    friends list
    friends headers

---
+++ SteadyFit/SteadyFit/SteadyFit/SteadyFit/HistogramViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HistogramViewController.swift
+//  SteadyFit
+//
+//  Created by Raheem Mian on 2018-11-03.
+//  Copyright © 2018 Raheem Mian. All rights reserved.
+//
+
+import UIKit
+
+class HistogramViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
---
+++ SteadyFit/SteadyFit/SteadyFit/SteadyFit/friendProfileViewController.swift
@@ -0,0 +1,18 @@
+//
+//  friendProfileViewController.swift
+//  SteadyFit
+//
+//  Created by Raheem Mian on 2018-11-03.
+//  Copyright © 2018 Raheem Mian. All rights reserved.
+//
+
+import UIKit
+
+class friendProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
