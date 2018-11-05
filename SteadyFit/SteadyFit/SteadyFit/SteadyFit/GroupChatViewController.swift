8a36089934202b5887867ae6471856ab7564a45a
Dickson Chum <dicksonchum@hotmail.com>
2018-11-04 23:56:08 -0800

    added GruopChatViewController

---
+++ SteadyFit/SteadyFit/SteadyFit/SteadyFit/GroupChatViewController.swift
@@ -0,0 +1,18 @@
+//
+//  GroupChatViewController.swift
+//  SteadyFit
+//
+//  Created by Dickson Chum on 2018-11-04.
+//  Copyright © 2018 Daycar. All rights reserved.
+//
+
+import UIKit
+
+class GroupChatViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
