b93c5d3402d621f6e65a29edd226b0fe2e9ce280
Amy090912 <35473816+Amy090912@users.noreply.github.com>
2018-11-15 17:15:25 -0800

    settingEditor

--- SteadyFit/SteadyFit/SteadyFit/SteadyFit/SettingsEditiorViewController.swift
+++ SteadyFit/SteadyFit/SteadyFit/SteadyFit/SettingsEditiorViewController.swift
@@ -20,15 +20,38 @@ class SettingsEditiorViewController: UIViewController {
     @IBAction func saveButton(_ sender: UIButton) {
 //        writeToFirebace()
     }
-    @IBAction func nameTextFile(_ sender: UITextField) {
-        
+    
+    
+    @IBOutlet var editProfileDatePicker: UITextField!
+    let datePicker = UIDatePicker()
+    
+    @IBAction func BirthdayTextField(_ sender: UITextField) {
     }
+   
+    @IBAction func CityTextField(_ sender: UITextField) {
+    }
+    
+    @IBAction func ProvinceTextField(_ sender: UITextField) {
+    }
+    
+    @IBAction func ActivityLevelTextField(_ sender: UITextField) {
+    }
+    
+    @IBAction func BioTextField(_ sender: UITextField) {
+    }
+    
+    @IBAction func GenderTextField(_ sender: UITextField) {
+    }
+    
     @IBAction func logoutButton(_ sender: UIButton) {
         // logout function
         
     }
     
-    @IBOutlet weak var nameLabel: UILabel!
+    //@IBOutlet weak var nameLabel: UILabel!
+
+    @IBOutlet weak var nameLabel: UIStackView!
+    
 
     
 //    func writeToFirebase(){
@@ -38,9 +61,30 @@ class SettingsEditiorViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        creatDataPicker()
+        
+        
         
         // Do any additional setup after loading the view.
     }
+    
+    func creatDataPicker()
+    {
+        editProfileDatePicker.inputView = datePicker
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: nil)
+        toolbar.setItems([doneButton], animated: true)
+        editProfileDatePicker.inputAccessoryView = toolbar
+    }
+    
+    func doneClick()
+    {
+        editProfileDatePicker.text = "\(datePicker.date)"
+        self.view.endEditing(true)
+    }
+    
 
     
 }
