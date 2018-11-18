b7c416c11ee306905700af8498dde9ec7a0c7fb9
Raheem Mian <rmian@asb9700-e05.csil.sfu.ca>
2018-11-17 16:53:52 -0800

    Calvin's success?

---
+++ SteadyFit/SteadyFit/SteadyFit/SteadyFit/Model/GroupEvents.swift
@@ -0,0 +1,14 @@
+//
+//  GroupEvents.swift
+//  SteadyFit
+//
+//  Created by Calvin Liu on 11/17/18.
+//  Copyright © 2018 Daycar. All rights reserved.
+//
+
+import UIKit
+
+class GroupEvents : NSObject {
+    var sessionId : String?
+    var eventName : String?
+}
