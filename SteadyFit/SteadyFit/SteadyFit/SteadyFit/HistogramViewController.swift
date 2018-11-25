a0092e9c10a54497d514329add771a29abee9778
alexachen <alexac@sfu.ca>
2018-11-25 02:11:18 -0800

    bug fixes for histogram

--- SteadyFit/SteadyFit/SteadyFit/SteadyFit/HistogramViewController.swift
+++ SteadyFit/SteadyFit/SteadyFit/SteadyFit/HistogramViewController.swift
@@ -18,9 +18,8 @@
 //  List of Changes: Change histogram bar height to dynamic height; populated histogram with data from firebase
 //
 //  List of known bugs:
-//  sorting of the date in weekly and monthly view is incorrect
-//  week difference calculation is incorrect, thus it will sometimes produce one week more than the current week
-//  month difference calculation is incorrect, thus it will sometimes produce one month more than the current month
+//  monthly view's text is squished together
+//  naming of the view switch buttons inconsistent and confusing
 
 
 import UIKit
@@ -64,12 +63,20 @@ class HistogramViewController: UIViewController {
             let colors = [#colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1), #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1), #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1), #colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1), #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1), #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1), #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)]
             var i:Int = 0
             for data in sorted_data {
+                var date = Date()
+                let currentDateFormatter = DateFormatter()
+                currentDateFormatter.dateFormat = "yyyy-MM-dd"
+                let currentDate = currentDateFormatter.date(from: data.key)
+                let currentDateComponents = Calendar.current.dateComponents([.year], from: currentDate!)
+                let todayDateComponents = Calendar.current.dateComponents([.year], from: date)
+                if (currentDateComponents.year! < todayDateComponents.year!){
+                    break;
+                }
                 let val = data.value
                 let height: Float = Float(val) / Float(find_max_value[0].value)
                 
                 let formatter = DateFormatter()
                 formatter.dateFormat = "d MMM"
-                var date = Date()
                 date.addTimeInterval(TimeInterval(-24*60*60*i))
                 result.append(BarEntry(color: colors[i % colors.count], height: height, textValue: "\(val)", title: formatter.string(from: date)))
                 i += 1
@@ -96,8 +103,9 @@ class HistogramViewController: UIViewController {
             
             var week_count:Int = 0
             
-            var oldest = startOfMonth
+            let oldest = startOfMonth
             let today = Date()
+            let todayComponents = Calendar.current.dateComponents([.weekOfMonth,.year,.month], from: today)
             let components = Calendar.current.dateComponents([.weekOfMonth], from: oldest!, to: today)
             week_count = (components.weekOfMonth ?? 0) + 1
             
@@ -105,7 +113,9 @@ class HistogramViewController: UIViewController {
             for i in 0...week_count{
                 let tempWeek:Date = Calendar.current.date(byAdding: .day, value: i*7, to: oldest!)!
                 let tempComponents = Calendar.current.dateComponents([.weekOfMonth,.year,.month], from: tempWeek)
-                
+                if (tempComponents.year! >= todayComponents.year! && tempComponents.month! >= todayComponents.month! && tempComponents.weekOfMonth! > todayComponents.weekOfMonth!){
+                    break;
+                }
                 let tempDateString = "week" + String(tempComponents.weekOfMonth!) + " of " + String(tempComponents.month!)+"," + String(tempComponents.year!)
                 weekly_data[tempDateString] = 0
             }
@@ -124,12 +134,12 @@ class HistogramViewController: UIViewController {
             let find_max_value = weekly_data.sorted { (aDic, bDic) -> Bool in
                 return aDic.value > bDic.value
             }
-            weekly_data.sorted { (aDic, bDic) -> Bool in
-                return aDic.key < bDic.key}
+            let sorted_weekly_data = weekly_data.sorted { (aDic, bDic) -> Bool in
+                return aDic.key > bDic.key}
             
             let colors = [#colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1), #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1), #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1), #colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1), #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1), #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1), #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)]
             var i:Int = 0
-            for data in weekly_data {
+            for data in sorted_weekly_data {
                 let val = data.value
                 let height: Float = Float(val) / Float(find_max_value[0].value)
                 var barTitle = data.key
@@ -158,6 +168,7 @@ class HistogramViewController: UIViewController {
             
             let oldest = dateFormatter.date(from: oldest_month)
             let today = Date()
+            let todayComponents = Calendar.current.dateComponents([.year,.month], from: today)
             let components = Calendar.current.dateComponents([.month], from: oldest!, to: today)
             month_count = (components.month ?? 0) + 1
             
@@ -165,18 +176,18 @@ class HistogramViewController: UIViewController {
             for i in 0...month_count{
                 let tempMonth:Date = Calendar.current.date(byAdding: .month, value: i, to: oldest!)!
                 let tempComponents = Calendar.current.dateComponents([.year,.month], from: tempMonth)
-                
-                let tempDateString = String(tempComponents.month!) + "," + String(tempComponents.year!)
+                if (tempComponents.year! >= todayComponents.year! && tempComponents.month! > todayComponents.month!){
+                    break;
+                }
+                let tempDateString = String(tempComponents.year!) + "," + String(format: "%02d",tempComponents.month!)
                 monthly_data[tempDateString] = 0
             }
-            monthly_data.sorted { (aDic, bDic) -> Bool in
-                return aDic.key > bDic.key
-            }
+
             for each in histogram_data{
                 let appendValue = each.value
                 let monthValue = dateFormatter.date(from: each.key)
                 var tempComponents = Calendar.current.dateComponents([.year,.month], from: monthValue!)
-                let formattedKey = String(tempComponents.month!) + "," + String(tempComponents.year!)
+                let formattedKey = String(tempComponents.year!) + "," + String(format: "%02d",tempComponents.month!)
                 monthly_data[formattedKey] = Int(monthly_data[formattedKey]!) + Int(appendValue)
             }
             
@@ -185,14 +196,15 @@ class HistogramViewController: UIViewController {
                 return aDic.value > bDic.value
             }
             
-            monthly_data.sorted { (aDic, bDic) -> Bool in
+            let sorted_monthly_data = monthly_data.sorted {$0.key.localizedStandardCompare($1.key) == .orderedDescending}
+            /*{ (aDic, bDic) -> Bool in
                 return aDic.key > bDic.key
-            }
+            }*/
             
             
             let colors = [#colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1), #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1), #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1), #colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1), #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1), #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1), #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)]
             var i:Int = 0
-            for data in monthly_data {
+            for data in sorted_monthly_data {
                 let val = data.value
                 let height: Float = Float(val) / Float(find_max_value[0].value)
                 result.append(BarEntry(color: colors[i % colors.count], height: height, textValue: "\(val)", title: data.key))
